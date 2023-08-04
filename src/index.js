2dbf7855e0eca50e7d5bee80b56bc3ee36782426
classr-22 <roundup414@gmail.com>
2023-08-05 00:06:39 +0530

    first commit created email form of netflix

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {NetflicksRegisterComponent} from './components/NetflicksRegisterComponent.js';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NetflicksRegisterComponent />
   </React.StrictMode>
 );
 
